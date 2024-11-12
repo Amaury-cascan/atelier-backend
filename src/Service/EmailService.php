72093b8c8b0b66b3f1dfbc48dca55882c649d96c
acascan <cascan@goyer.fr>
2024-11-12 21:31:17 +0100

    gestion des mails

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -25,12 +25,16 @@ class EmailService
             ->from(new Address($this->fromEmail, $this->fromName))
             ->to($to)
             ->subject("Bienvenue sur L'atelier de Marie !")
-            ->text("Bonjour et bienvenue, $username !\n\n"
-                . "Nous vous remercions d'avoir créé un compte.\n\n"
-                . "Vous pouvez dès à présent prendre rendez-vous facilement via notre site et profiter de nos prestations.\n\n"
-                . "Nous sommes ravis de vous avoir parmi nous et restons à votre disposition pour toute question.\n\n"
-                . "À très bientôt !");
-
+            ->html(
+                "<p>Bonjour et bienvenue, $username !</p>" .
+                "<p>Nous vous remercions d'avoir créé un compte.</p>" .
+                "<p>Vous pouvez dès à présent prendre rendez-vous facilement via notre site et profiter de nos prestations.</p>" .
+                "<p>Nous sommes ravis de vous avoir parmi nous et restons à votre disposition pour toute question.</p>" .
+                "<p>À très bientôt !</p>" .
+                "<p><strong>L'atelier de Marie</strong><br>" .
+                "06.60.53.50.44</p>" .
+                "<img src='https://www.backoffice.atelier-de-marie.com/images/1.ico' alt='Atelier de Marie' />"
+            );
 
         try {
             $this->mailer->send($email);
@@ -42,7 +46,7 @@ class EmailService
     {
         $email = (new TemplatedEmail())
             ->from(new Address($this->fromEmail, $this->fromName))
-            ->to("latelierdemarie41@outlook.com")
+            ->to("latelierdemarie41@outlook.com", "marie.pacreau14@outlook.fr")
             ->subject("Nouveau client !")
             ->text("Bonjour,\n\n"
                 . "Un nouveau client a créé un compte.\n\n"
